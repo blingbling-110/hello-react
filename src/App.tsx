cee2187ce3d377c8a70ec05a0a7e53108db5acba
qinzijun <qinzijun@hongwuniu.com>
2021-11-29 16:47:47 +0800

    feat: 完成TabCtrl

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TabCtrl from './components/TabCtrl'
+import { Component } from 'react'
 
-function App() {
-  return (
+interface IAppState {
+  currView: JSX.Element
+}
+
+export default class App extends Component<any, IAppState> {
+  tabContents: JSX.Element[];
+
+  constructor (props: object) {
+    super(props)
+
+    this.state = {
+      currView: this.defApp
+    }
+
+    this.tabContents = [this.defApp, <h2>customApp</h2>]
+  }
+
+  private defApp = (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,7 +37,24 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
+  )
 
-export default App;
+  render () {
+    const { currView } = this.state
+
+    return (
+      <div className="app-wrapper">
+        <div className="tab-view">
+          {currView}
+        </div>
+        <TabCtrl tabTitles={['缺省', '自定义']} chgView={index => this.chgView(index)}/>
+      </div>
+    )
+  }
+
+  chgView (index: number) {
+    this.setState({
+      currView: this.tabContents[index]
+    })
+  }
+}
---
+++ src/components/TabCtrl/index.tsx
@@ -0,0 +1,42 @@
+import { Component } from 'react'
+import './index.scss'
+
+interface ITabProps {
+  tabTitles: string[],
+  chgView: (index: number) => void
+}
+
+interface ITabState {
+  currIndex: number
+}
+
+export default class TabCtrl extends Component<ITabProps, ITabState> {
+  constructor (props: ITabProps) {
+    super(props)
+
+    this.state = {
+      currIndex: 0
+    }
+  }
+
+  render () {
+    const { currIndex } = this.state
+
+    return (
+      <div className="tab-control">
+        {
+          this.props.tabTitles.map((tabTitle, index) =>
+            <div key={index} className={`tab-item ${index === currIndex ? 'active' : ''}`}
+                 onClick={() => this.chgTab(index)}>{tabTitle}</div>)
+        }
+      </div>
+    )
+  }
+
+  chgTab (index: number) {
+    this.setState({
+      currIndex: index
+    })
+    this.props.chgView(index)
+  }
+}
