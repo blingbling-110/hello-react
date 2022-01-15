c27b488e753e5d466b5d3ba19c49ce8935373303
blingbling-110 <523497359@qq.com>
2022-01-15 23:59:02 +0800

    feat: react-transition-group, 纯函数

--- src/components/AntD.tsx
+++ src/components/AntD.tsx
@@ -1,20 +1,77 @@
 import { PureComponent } from "react";
-import { Button, DatePicker, Space } from 'antd';
+import { Button, Card, DatePicker, Space } from 'antd';
 import { PoweroffOutlined } from '@ant-design/icons';
 import moment from "moment";
-import { CommentItem } from "./Comment/CommentItem";
+import { CommentItem, COM_ITEM_TIME_OUT } from "./Comment/CommentItem";
 import { CommentInput, ICommentInfo } from "./Comment/CommentInput";
 import request, { requestAll } from "../service/Ajax";
+import { CSSTransition, SwitchTransition, TransitionGroup } from 'react-transition-group'
+import styled from "styled-components";
 
 interface IAntDState {
   loadings: boolean[],
-  commentInfos: ICommentInfo[]
+  commentInfos: ICommentInfo[],
+  showCard: boolean,
+  showOn: boolean
 }
+
+const CARD_TIME_OUT = 1000
+const BTN_TIME_OUT = 500
+
+const StyledCard = styled(Card)`
+  display: inline-block;
+
+  &.card-enter, &.card-appear {
+    opacity: 0;
+  }
+
+  &.card-enter-active, &.card-appear-active {
+    opacity: 1;
+    transition: opacity ${CARD_TIME_OUT}ms;
+  }
+
+  &.card-enter-done, &.card-appear-done {
+    opacity: 1;
+  }
+
+  &.card-exit {
+    opacity: 1;
+  }
+
+  &.card-exit-active {
+    opacity: 0;
+    transition: opacity ${CARD_TIME_OUT}ms;
+  }
+
+  &.card-exit-done {
+    opacity: 0;
+  }
+`
+
+const StyledBtn = styled(Button)`
+  &.btn-enter {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+
+  &.btn-enter-active {
+    opacity: 1;
+    transition: opacity ${BTN_TIME_OUT}ms, transform ${BTN_TIME_OUT}ms;
+  }
+
+  &.btn-exit-active {
+    opacity: 0;
+    transform: translateX(-100%);
+    transition: opacity ${BTN_TIME_OUT}ms, transform ${BTN_TIME_OUT}ms;
+  }
+`
 
 export class AntD extends PureComponent<any, IAntDState> {
   state: IAntDState = {
     loadings: [],
-    commentInfos: []
+    commentInfos: [],
+    showCard: true,
+    showOn: true
   };
 
   enterLoading = (index: number) => {
@@ -39,7 +96,7 @@ export class AntD extends PureComponent<any, IAntDState> {
   };
 
   render () {
-    const { loadings } = this.state;
+    const { loadings, showCard, showOn } = this.state;
     return (
       // 没有顶栏因此添加maxHeight: 100%
       <div style={{ maxHeight: '100%', padding: '20px' }}>
@@ -75,19 +132,69 @@ export class AntD extends PureComponent<any, IAntDState> {
 
         <DatePicker defaultValue={moment('2022-01-03', 'YYYY-MM-DD')} allowClear={false}/>
 
-        {this.state.commentInfos.map(commentInfo =>
-          <CommentItem key={commentInfo.id} info={commentInfo} delete={() => this.deleteComment(commentInfo.id)}/>
-        )}
+        <TransitionGroup>
+          {
+            this.state.commentInfos.map(commentInfo =>
+              <CSSTransition
+                key={commentInfo.id}
+                timeout={COM_ITEM_TIME_OUT}
+                classNames={'com-item'}
+              >
+                <CommentItem info={commentInfo} delete={() => this.deleteComment(commentInfo.id)}/>
+              </CSSTransition>
+            )
+          }
+        </TransitionGroup>
 
         <CommentInput addComment={(info: ICommentInfo) => this.setState({
           commentInfos: [...this.state.commentInfos, info]
         })}/>
+        <Button type="primary" onClick={() => this.setState({ showCard: !showCard })}>显示/隐藏</Button>
+
+        <CSSTransition
+          in={this.state.showCard}
+          classNames={'card'}
+          timeout={CARD_TIME_OUT}
+          // unmountOnExit={true}
+          appear
+          onEnter={(el: any) => console.log('onEnter', el)}
+          onEntering={(el: any) => console.log('onEntering', el)}
+          onEntered={(el: any) => console.log('onEntered', el)}
+          onExit={(el: any) => console.log('onExit', el)}
+          onExiting={(el: any) => console.log('onExiting', el)}
+          onExited={(el: any) => console.log('onExited', el)}
+        >
+          <StyledCard
+            hoverable
+            style={{ width: 240 }}
+            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
+          >
+            <Card.Meta title="Europe Street beat" description="www.instagram.com"/>
+          </StyledCard>
+        </CSSTransition>
+
+        <SwitchTransition mode={"out-in"}>
+          <CSSTransition
+            key={showOn ? 'on' : 'off'}
+            classNames={'btn'}
+            timeout={BTN_TIME_OUT}
+          >
+            <StyledBtn onClick={() => this.setState({ showOn: !showOn })}>
+              {showOn ? 'on' : 'off'}
+            </StyledBtn>
+          </CSSTransition>
+        </SwitchTransition>
       </div>
     )
   }
 
   private deleteComment (id: number) {
     const index = this.state.commentInfos.findIndex(commentInfo => commentInfo.id === id)
+    if (index === -1)
+    {
+      // 若未找到则直接返回
+      return
+    }
     const restCommentInfos = [...this.state.commentInfos]
     restCommentInfos.splice(index, 1)
     this.setState({
--- src/components/Comment/CommentItem.tsx
+++ src/components/Comment/CommentItem.tsx
@@ -2,18 +2,37 @@ import { PureComponent } from "react";
 import { ICommentInfo } from "./CommentInput";
 import { Avatar, Comment, Tooltip } from 'antd'
 import { DeleteOutlined } from "@ant-design/icons";
+import styled from "styled-components";
 
 interface ICommentItemProps {
   info: ICommentInfo,
   delete: () => any
 }
 
+export const COM_ITEM_TIME_OUT = 888
+
+const StyledComItem = styled(Comment)`
+  &.com-item-enter {
+    opacity: 0;
+  }
+
+  &.com-item-enter-active {
+    opacity: 1;
+    transition: opacity ${COM_ITEM_TIME_OUT}ms;
+  }
+
+  &.com-item-exit-active {
+    opacity: 0;
+    transition: opacity ${COM_ITEM_TIME_OUT}ms;
+  }
+`
+
 export class CommentItem extends PureComponent<ICommentItemProps> {
   render () {
     const { avatar, username, time, content } = this.props.info
 
     return (
-      <Comment
+      <StyledComItem
         actions={[<span onClick={() => this.props.delete()}><DeleteOutlined/>删除</span>]}
         author={username}
         avatar={<Avatar src={avatar} alt={username}/>}
